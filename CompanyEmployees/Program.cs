df4a02bafd371d19f8ea99838b851279566dbcc6
Urvish <webbyurvish@gmail.com>
2024-05-09 00:04:54 +0530

    Add project files.

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,45 @@
+// Builder usages :
+
+//Adding Configuration to the project by using the builder.Configuration property
+//Registering services in our app with the builder.Services property
+//Logging configuration with the builder.Logging property
+//Other IHostBuilder and IWebHostBuilder configuration
+
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerServices();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
