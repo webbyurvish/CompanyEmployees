091b01484d19924ea206feb494b9f4da40b981d5
Urvish <webbyurvish@gmail.com>
2024-05-13 23:36:45 +0530

    Added Presentation Layer and configured Get all Companies

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,5 +1,7 @@
 ﻿using Contracts;
+using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,22 @@ namespace Service
         {
             _repository = repository;
             _logger = logger;
+        }
+
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges);
+
+                var companiesDto = companies.Select(c => new CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country))).ToList();
+                return companiesDto;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
+                throw;
+            }
         }
     }
 }
