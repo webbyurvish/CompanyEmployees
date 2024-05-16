0bb1afa23f085e9fb00e4a9cf809b9d7e8a48554
Urvish <webbyurvish@gmail.com>
2024-05-16 18:37:42 +0530

    Completed Content Negotiation with csv type

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddControllers(config =>
     config.RespectBrowserAcceptHeader = true;
     config.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters()
+.AddCustomCSVFormatter()
 .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
