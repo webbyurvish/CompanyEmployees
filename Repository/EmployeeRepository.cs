9021aaf63a07d2f795c01e5d47f8e181a55e5349
Urvish <webbyurvish@gmail.com>
2024-05-20 19:35:02 +0530

    Implemented Searching

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -18,7 +19,9 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
+              .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+              .Search(employeeParameters.SearchTerm)
               .OrderBy(e => e.Name)
               .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
               .Take(employeeParameters.PageSize)
